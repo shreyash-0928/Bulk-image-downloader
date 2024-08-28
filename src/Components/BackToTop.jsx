6e2e1f468f5867077dbdac653dfb81b0a381aff7
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-08-28 11:47:04 +0530

    back to top component

--- src/Components/BackToTop.jsx
+++ src/Components/BackToTop.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import UpArrow from "../assets/arrow-up.svg"
+import React from "react";
+import UpArrow from "../assets/arrow-up.svg";
 
 function BackToTop() {
-  function backToTop(){
+  function backToTop() {
     window.scrollTo({
-        top:0,
-        left:0,
-        behavior : "smooth"
-    })
-}
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }
 
   return (
-    <div 
-    onClick={backToTop}
-    className='back-to-top-container'>
-        <img src={UpArrow} alt="go back to the top of the page" />
+    <div onClick={backToTop} className="back-to-top-container">
+      <img src={UpArrow} alt="go back to the top of the page" />
     </div>
-  )
+  );
 }
 
-export default BackToTop
+export default BackToTop;
