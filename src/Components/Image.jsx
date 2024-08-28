5f7afbc61e0171a7f0a8840f39ae55bc7b6d60f5
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-08-28 11:49:10 +0530

    image component

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,18 +1,18 @@
-function Image({src, color, setImagePreviewSrc,setBgColor, description}) {
-
-  function handleImageClick(src, color){
-    setImagePreviewSrc(src)
-    setBgColor(color)
+function Image({ src, color, setImagePreviewSrc, setBgColor, description }) {
+  function handleImageClick(src, color) {
+    setImagePreviewSrc(src);
+    setBgColor(color);
   }
   return (
-    <div 
-    onClick={()=>{
-      handleImageClick(src, color)
-    }}
-    className="pack-image-container">
-             <img src={src} alt={description || "image"} />
-          </div>
-  )
+    <div
+      onClick={() => {
+        handleImageClick(src, color);
+      }}
+      className="pack-image-container"
+    >
+      <img src={src} alt={description || "image"} />
+    </div>
+  );
 }
 
-export default Image
+export default Image;
